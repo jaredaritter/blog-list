fd9594340c647707b7c461eb5c706e00de6862f1
jared <38332939+jaredaritter@users.noreply.github.com>
2021-07-12 16:03:09 -0400

    Move blog model to separate module.

--- index.js
+++ index.js
@@ -3,25 +3,9 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 const config = require('./utils/config');
 const logger = require('./utils/logger');
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-blogSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  },
-});
-
-const Blog = mongoose.model('Blog', blogSchema);
 
 logger.info('Connecting to MongoDB');
 mongoose
@@ -51,7 +35,6 @@ app.post('/api/blogs', (request, response) => {
   });
 });
 
-const PORT = 3003;
-app.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`);
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
 });
