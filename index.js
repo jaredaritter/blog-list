ea677c672db90cf07852bad5365a4dec3308c058
jared <38332939+jaredaritter@users.noreply.github.com>
2021-07-12 16:10:07 -0400

    Moved blog routes to their own router.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
+const blogsRouter = require('./controllers/blogs');
 const Blog = require('./models/blog');
 const config = require('./utils/config');
 const logger = require('./utils/logger');
@@ -21,19 +22,7 @@ mongoose
 app.use(cors());
 app.use(express.json());
 
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
+app.use('/api/blogs', blogsRouter);
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
