ac8eea0c0daf077d2929d17a88f0086209e47b9e
jared <38332939+jaredaritter@users.noreply.github.com>
2021-07-12 15:58:09 -0400

    Initial commit.

---
+++ index.js
@@ -0,0 +1,57 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const config = require('./utils/config');
+const logger = require('./utils/logger');
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+});
+
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+const Blog = mongoose.model('Blog', blogSchema);
+
+logger.info('Connecting to MongoDB');
+mongoose
+  .connect(config.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => logger.info('Connected to DB'))
+  .catch((error) => logger.error(error));
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/blogs', (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs);
+  });
+});
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body);
+
+  blog.save().then((result) => {
+    response.status(201).json(result);
+  });
+});
+
+const PORT = 3003;
+app.listen(PORT, () => {
+  logger.info(`Server running on port ${PORT}`);
+});
